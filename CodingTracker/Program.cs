3708ea949afd5423766e1cffc6400e2d293fe48d
Santiago Sica <santisica29@gmail.com>
2025-05-26 09:59:34 -0300

    create database

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -8,7 +8,14 @@ using (var connection = new SqliteConnection(connectionString))
     connection.Open();
     var tableCmd = connection.CreateCommand();
 
-    tableCmd.CommandText = "";
+    tableCmd.CommandText = 
+        @"CREATE TABLE IF NOT EXISTS coding_tracker (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            StartTime TEXT,
+            EndTime TEXT,
+            Duration INTEGER
+            )";
 
     tableCmd.ExecuteNonQuery();
 }
