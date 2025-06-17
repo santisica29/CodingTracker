a549cbd96edca4d7e27c7ffdf48a66b9de69d2c8
Santiago Sica <santisica29@gmail.com>
2025-06-17 19:21:00 -0300

    re order some methods so it looks cleaner

--- CodingTracker/Controller/CodingController.cs
+++ CodingTracker/Controller/CodingController.cs
@@ -9,117 +9,6 @@ using System.Globalization;
 namespace CodingTracker.Controller;
 internal class CodingController : BaseController
 {
-    public void StartSession()
-    {
-        DisplayMessage("Session started", "blue");
-        var startTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
-
-        var timer = Helpers.RunStopWatch();
-
-        var choice = AnsiConsole.Confirm("Do you want to save your session?");
-
-        if (!choice) return;
-
-        var endTime = timer;
-
-        AddSession(startTime, endTime);
-    }
-
-    public void ViewReportOfCodingSession()
-    {
-        DisplayMessage("Get your coding tracker report!");
-
-        var choice = AnsiConsole.Prompt(
-            new SelectionPrompt<ReportOption>()
-            .Title("Select your report type")
-            .AddChoices(Enum.GetValues<ReportOption>())
-            );
-
-        string? unit = null;
-        if (choice != ReportOption.Total)
-        {
-            unit = AnsiConsole.Prompt(
-                new TextPrompt<string>($"Select the number of {choice} for your report."));
-        }
-
-        // send choice as arg on method that gets a sql sql
-        var listOfReport = GetReport(choice, unit);
-
-        var additionalList = GetReportOfTotalAndAvg(choice, unit);
-        // maybe refactor getSessions method to do it there
-        // recieve list
-        ViewSessions(listOfReport, additionalList);
-        // do another sql to figure out the total and average duration
-        // display it
-    }
-
-    public List<CodingSession>? GetReport(ReportOption choice, string unit)
-    {
-        var sql = $"SELECT * FROM coding_tracker ";
-
-        _ = choice switch
-        {
-            ReportOption.Days => sql += $"WHERE EndTime > date('now', '-{unit} days')",
-            ReportOption.Months => sql += $"WHERE EndTime > date('now','start of month', '-{unit} months')",
-            ReportOption.Years => sql += $"WHERE EndTime > date('now','start of year', '-{unit} years')",
-            ReportOption.Total => sql
-        };
-
-        sql += " ORDER BY StartTime DESC";
-
-        return GetSessions(sql);
-    }
-
-    public List<string>? GetReportOfTotalAndAvg(ReportOption choice, string unit)
-    {
-        using var connection = new SqliteConnection(DatabaseInitializer.GetConnectionString());
-
-        var sql = $"SELECT Duration FROM coding_tracker ";
-        _ = choice switch
-        {
-            ReportOption.Days => sql += $"WHERE EndTime > date('now', '-{unit} days')",
-            ReportOption.Months => sql += $"WHERE EndTime > date('now','start of month', '-{unit} months')",
-            ReportOption.Years => sql += $"WHERE EndTime > date('now','start of year', '-{unit} years')",
-            ReportOption.Total => sql
-        };
-
-        sql += " ORDER BY StartTime DESC";
-
-        var list = connection.Query<string>(sql).ToList();
-
-        return list;
-    }
-
-
-    public List<CodingSession>? GetSessions(string? sql = null)
-    {
-        using var connection = new SqliteConnection(DatabaseInitializer.GetConnectionString());
-
-        if (sql == null) sql = $"SELECT * FROM coding_tracker";
-
-        var listFromDB = connection.Query(sql).ToList();
-
-        if (listFromDB.Count == 0) return null;
-
-        var listOfCodingSessions = Helpers.ParseAnonObjToCodingSession(listFromDB);
-
-        return listOfCodingSessions;
-    }
-    public void ViewSessions(List<CodingSession>? list = null, List<string> additionalList = null)
-    {
-        Helpers.CheckIfListIsNullOrEmpty(list);
-
-        Helpers.CreateTable(list, ["ID", "Start Time", "End Time", "Duration"]);
-
-        if(additionalList != null)
-        {
-            Helpers.CreateTableOfAvg(additionalList);
-        }
-
-        AnsiConsole.MarkupLine("Press Any Key to Continue.");
-        Console.ReadKey();
-    }
-
     public void AddSession(string? startTime = null, string? endTime = null)
     {
         if (startTime == null || endTime == null)
@@ -144,7 +33,7 @@ internal class CodingController : BaseController
             @$"INSERT INTO {DatabaseInitializer.GetDBName()} (startTime, endTime, duration)
                VALUES (@StartTime, @EndTime, @Duration)";
 
-        var affectedRows = connection.Execute(sql, new { StartTime = startTime, EndTime = endTime, Duration = session.CalculateDuration().ToString()});
+        var affectedRows = connection.Execute(sql, new { StartTime = startTime, EndTime = endTime, Duration = session.CalculateDuration().ToString() });
 
         if (affectedRows > 0) DisplayMessage("Addition completed.", "green");
         else DisplayMessage("No changes made");
@@ -224,4 +113,111 @@ internal class CodingController : BaseController
         AnsiConsole.MarkupLine("Press any key to continue.");
         Console.ReadKey();
     }
+    public void StartSession()
+    {
+        DisplayMessage("Session started", "blue");
+        var startTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+
+        var timer = Helpers.RunStopWatch();
+
+        var choice = AnsiConsole.Confirm("Do you want to save your session?");
+
+        if (!choice) return;
+
+        var endTime = timer;
+
+        AddSession(startTime, endTime);
+    }
+
+    public List<CodingSession>? GetSessions(string? sql = null)
+    {
+        using var connection = new SqliteConnection(DatabaseInitializer.GetConnectionString());
+
+        if (sql == null) sql = $"SELECT * FROM coding_tracker";
+
+        var listFromDB = connection.Query(sql).ToList();
+
+        if (listFromDB.Count == 0) return null;
+
+        var listOfCodingSessions = Helpers.ParseAnonObjToCodingSession(listFromDB);
+
+        return listOfCodingSessions;
+    }
+
+    public void ViewSessions(List<CodingSession>? list = null, List<string> additionalList = null)
+    {
+        Helpers.CheckIfListIsNullOrEmpty(list);
+
+        Helpers.CreateTable(list, ["ID", "Start Time", "End Time", "Duration"]);
+
+        if (additionalList != null)
+        {
+            Helpers.CreateTableOfAvg(additionalList);
+        }
+
+        AnsiConsole.MarkupLine("Press Any Key to Continue.");
+        Console.ReadKey();
+    }
+
+    public void ViewReportOfCodingSession()
+    {
+        DisplayMessage("Get your coding tracker report!");
+
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<ReportOption>()
+            .Title("Select your report type")
+            .AddChoices(Enum.GetValues<ReportOption>())
+            );
+
+        string? unit = null;
+
+        if (choice != ReportOption.Total)
+        {
+            unit = AnsiConsole.Prompt(
+                new TextPrompt<string>($"Select the number of {choice} for your report."));
+        }
+
+        var listOfReport = GetReport(choice, unit);
+
+        var additionalList = GetReportOfTotalAndAvg(choice, unit);
+
+        ViewSessions(listOfReport, additionalList);
+    }
+
+    public List<CodingSession>? GetReport(ReportOption choice, string? unit)
+    {
+        var sql = $"SELECT * FROM coding_tracker ";
+
+        _ = choice switch
+        {
+            ReportOption.Days => sql += $"WHERE EndTime > date('now', '-{unit} days')",
+            ReportOption.Months => sql += $"WHERE EndTime > date('now','start of month', '-{unit} months')",
+            ReportOption.Years => sql += $"WHERE EndTime > date('now','start of year', '-{unit} years')",
+            ReportOption.Total => sql
+        };
+
+        sql += " ORDER BY StartTime DESC";
+
+        return GetSessions(sql);
+    }
+
+    public List<string>? GetReportOfTotalAndAvg(ReportOption choice, string? unit)
+    {
+        using var connection = new SqliteConnection(DatabaseInitializer.GetConnectionString());
+
+        var sql = $"SELECT Duration FROM coding_tracker ";
+        _ = choice switch
+        {
+            ReportOption.Days => sql += $"WHERE EndTime > date('now', '-{unit} days')",
+            ReportOption.Months => sql += $"WHERE EndTime > date('now','start of month', '-{unit} months')",
+            ReportOption.Years => sql += $"WHERE EndTime > date('now','start of year', '-{unit} years')",
+            ReportOption.Total => sql
+        };
+
+        sql += " ORDER BY StartTime DESC";
+
+        var list = connection.Query<string>(sql).ToList();
+
+        return list;
+    }  
 }
